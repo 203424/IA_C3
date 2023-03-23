41ab060e07065bae25b41c330c989729496829a1
Gabriel <gabrielramondev23@gmail.com>
2023-03-22 22:06:45 -0600

    cambios en el proyecto

--- genetic_algorithm/main.py
+++ genetic_algorithm/main.py
@@ -1,106 +1,14 @@
-import tensorflow as tf
-import os
-import numpy as np
-from PIL import Image
-from sklearn.model_selection import KFold
 from random import randint, uniform, random, sample
 from itertools import combinations, product
-class neuronal_network:
-    def __init__(self):
-        self.classification = os.listdir('data/train')
-        self.images = []
-        self.num_layers = 0
-        self.num_filters = 0
-        self.num_classes = 0
-        self.learning_rate = 0
-        self.model = None
-        self.labels = []
-        self.history_list = []
-        self.accuracy_list = []
-        self.num_folds = 0
-
-    def preprocess_images(self):
-        x = 0
-        for dirr in self.classification:
-            for image in os.listdir('./data/train/' + dirr):
-                img = Image.open('./data/train/' + dirr + '/' + image).convert('RGB').resize((30,30),)
-                img = np.asanyarray(img)
-                self.images.append(img) 
-                self.labels.append(x)
-            x += 1  
-            self.num_classes += 1   
-            
-        self.images = np.asanyarray(self.images)
-        self.labels = np.asanyarray(self.labels)
-        self.images = self.images / 255
-
-    def define_model(self, num_layers_dense, num_neurons, activation):
-        #Datos que pasa el individuo
-        self.num_layers = 4
-        self.num_filters = 16
-        self.learning_rate = 0.005
-        
-        model = tf.keras.Sequential()
-        model.add(tf.keras.layers.Conv2D(self.num_filters, (3, 3), input_shape=(30, 30, 3), activation='relu'))
-        
-        for i in range(self.num_layers - 1):
-            model.add(tf.keras.layers.Conv2D(self.num_filters, (3, 3), activation='relu'),)
-            
-        model.add(tf.keras.layers.MaxPooling2D(2, 2),)
-        model.add(tf.keras.layers.Flatten())
-        for i in range(num_layers_dense):
-            model.add(tf.keras.layers.Dense(num_neurons[i], activation=activation[i]))
-            
-        model.add(tf.keras.layers.Dense(self.num_classes, activation='softmax'))
-        
-        opt = tf.keras.optimizers.Adam(learning_rate=self.learning_rate)
-        model.compile(
-            optimizer = opt,
-            loss = tf.losses.SparseCategoricalCrossentropy(),
-            metrics = ['accuracy']
-        )        
-        self.model = model
-            
-    def train_model(self):
-        self.num_folds = 3
-        
-        kfold = KFold(n_splits=self.num_folds, shuffle=True, random_state=7)
-
-        for fold, (train_index, val_index) in enumerate(kfold.split(self.images, self.labels)):
-            #print(f'Train model for {fold} fold...')
-            x_train, y_train = self.images[train_index], self.labels[train_index]
-            x_val, y_val = self.images[val_index], self.labels[val_index]
-
-            history = self.model.fit(x_train, y_train, epochs=3, validation_data=(x_val, y_val), verbose=0)
-            loss, accurancy = self.model.evaluate(x_val, y_val, verbose=0)
-
-            self.history_list.append(history)
-            self.accuracy_list.append(accurancy*100)
-            
-    def save_model(self):
-        target_dir = './model/'
-        
-        if not os.path.exists(target_dir):
-            os.mkdir(target_dir)
-            
-        self.model.save('./model/model.h5')
-        self.model.save_weights('./model/weights.h5')
-        
-    def evaluate(self,num_layers_dense,num_neurons,activation):
-        self.preprocess_images()
-        self.define_model(num_layers_dense,num_neurons,activation)
-        self.train_model()
-        self.save_model()
-
 import numpy as np
-import sys, os
+import sys
 sys.path.append("./neuronal_network") 
 from neuronal_network import neuronal_network
 
 class genetic_algorithm:
     def __init__(self, num_layers, num_neurons,pop_size,num_generations,mut_rate, mut_rate_pos, mut_rate_layer, mut_rate_neurons,mut_rate_f_activation,num_trains, nn):
-        self.num_layers = num_layers #rango ej. (3,6)
-        self.num_neurons = num_neurons #rango ej. (64,256)
+        self.num_layers = num_layers 
+        self.num_neurons = num_neurons 
         self.function = ['relu','softmax']
         self.pop_initial = randint(2,pop_size)
         self.pop_size = pop_size
@@ -194,9 +102,6 @@ class genetic_algorithm:
         for child in self.children:
             if random() < self.mutation_rate:
                 child = self.mutate_child(child)
-
-            self.fitness.append(self.calculate_aptitude(child))
-
             self.fitness.append(self.calculate_aptitude(child))
             self.population.append(child)
 
@@ -251,17 +156,9 @@ class genetic_algorithm:
             print("Generation", generation+1, "- Best fitness:", self.fitness[0])
         print("mejor individuo", self.population[0])
 
-    # def show_result(self):
-    #     pass
-        print("mejor individuo", self.population[0])
-
     def show_result(self):
         #Mostrar resultados
         pass
-        
-        
-nn = neuronal_network()
-nn.preprocess_images()
 #Ejecutar AG
 nn = neuronal_network()
 nn.preprocess_images()
